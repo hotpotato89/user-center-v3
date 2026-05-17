884710b9acf35a80e282a9ddf55c0dc8b433e67b
hotpotato <ramilreading@gmail.com>
2026-05-17 20:24:42 +0300

    feat: добавил reload в uvicorn.run

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 
+from app.core.config import settings
+
 app = FastAPI(title='User Center V3')
 
 if __name__ == '__main__':
-    uvicorn.run('run:app', host='0.0.0.0', port=8000)
+    uvicorn.run(
+        'run:app',
+        host='0.0.0.0',
+        port=8000,
+        reload=settings.debug #type: ignore
+    )
