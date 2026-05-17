5afec07c586e5ec984e433f7b04a032c969adce6
hotpotato <ramilreading@gmail.com>
2026-05-17 20:33:33 +0300

    feat: добавил health роут

--- run.py
+++ run.py
@@ -3,7 +3,11 @@ import uvicorn
 
 from app.core.config import settings
 
+from app.api.health import router as health_router
+
 app = FastAPI(title='User Center V3')
+
+app.include_router(health_router)
 
 if __name__ == '__main__':
     uvicorn.run(
