15640df447d19d586ed902a61e8fa1cc5863cbb1
hotpotato <ramilreading@gmail.com>
2026-05-18 19:44:47 +0300

    docs: добавил docstring

--- app/core/database.py
+++ app/core/database.py
@@ -5,9 +5,11 @@ from app.core.config import settings
 DSN = f'postgresql://{settings.db_user}:{settings.db_password}@db/{settings.db_name}'
 
 async def create_connection():
+    """Создаем пул соединений"""
     return await asyncpg.create_pool(DSN, min_size=2, max_size=10)
 
 async def init_db(pool: asyncpg.Pool):
+    """Инициализируем таблицу"""
     async with pool.acquire() as conn:
         await conn.execute('create table if not exists users (id serial primary key, name varchar(60), age integer, email varchar(45) unique, reg_time timestamp default now())')
         await conn.execute('create index if not exists idx_regtime_desc on users(reg_time desc)')
