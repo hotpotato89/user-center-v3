60fbbe445b5af81d12b5e705b54225fa223e1529
hotpotato <ramilreading@gmail.com>
2026-05-18 20:46:58 +0300

    refactor: скопировал из V2

--- app/services/users_services.py
+++ app/services/users_services.py
@@ -2,19 +2,12 @@ import asyncpg
 
 from app.schemas.common import ReturnForm
 
-async def get_users_service(pool: asyncpg.Pool) -> ReturnForm:
+async def get_users_service(pool: asyncpg.Pool, limit: int, skip: int):
     async with pool.acquire() as conn:
-        data = await conn.fetch('select * from users')
-    
+        data = await conn.fetch('select * from users order by reg_time desc offset $1 limit $2', skip, limit)
+        total = await conn.fetchval('select count(*) from users')
+    current_page = (skip // limit) + 1 if limit>0 else 1
+    total_pages = (total + limit - 1) // limit if limit > 0 else 1
     if not data:
-        return ReturnForm(
-            success=False,
-            message='База данных пуста',
-            error_code='empty',
-        )
-
-    return ReturnForm(
-        success=True,
-        message='Данные получены успешно!',
-        data=[dict(row) for row in data],
-    )
+        return ReturnForm(success=False, message='База данных пуста', error_code='empty')
+    return ReturnForm(success=True, message=f'Найдено {len(data)} пользователей.', data=[dict(row) for row in data], total=total, page=current_page, total_pages=total_pages, limit=limit)
