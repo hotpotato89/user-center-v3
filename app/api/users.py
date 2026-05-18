ee3ade36caf367fc2c52228f4e3679a6bd5fd96a
hotpotato <ramilreading@gmail.com>
2026-05-18 20:47:40 +0300

    refactor: скопировал из V2

--- app/api/users.py
+++ app/api/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from typing import Annotated
 from asyncpg import Pool
 
@@ -9,10 +9,15 @@ from app.api.deps import get_pool
 router = APIRouter()
 
 @router.get('/users')
-async def get_users(pool: Annotated[Pool, Depends(get_pool)]):
-    result = await get_users_service(pool)
+async def get_users(pool: Annotated[Pool, Depends(get_pool)],
+                    limit: int = Query(..., ge=1, description='Лимит записей в одной странице'),
+                    page: int = Query(..., ge=1, description='Какая страница будет просмотрена')):
+    limit = min(limit, 50)
+    skip = (page-1)*limit
+
+    result = await get_users_service(pool=pool, limit=limit, skip=skip)
     if not result.success:
         if result.error_code == 'empty':
             raise HTTPException(status_code=404, detail=result.message)
-        raise HTTPException(status_code=500, detail='Внутрення ошибка сервера')
+        raise HTTPException(status_code=500, detail=result.message)
     return result
