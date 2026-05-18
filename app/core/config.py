b206ff946949c6fc42b52a45deb7ec1d1f771a19
hotpotato <ramilreading@gmail.com>
2026-05-18 19:40:15 +0300

    feat: дополнил конфиг

--- app/core/config.py
+++ app/core/config.py
@@ -4,10 +4,13 @@ from typing import Optional
 
 class Settings(BaseSettings):
     debug: Optional[bool] = False
+    db_name: str
+    db_user: str
+    db_password: str
 
     model_config = {
         'env_file': '.env',
         'extra': 'ignore',
     }
 
-settings = Settings()
+settings = Settings() #type: ignore
