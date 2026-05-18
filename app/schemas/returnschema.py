9d9e24661f9432441983105ee2b07d209e9f372b
hotpotato <ramilreading@gmail.com>
2026-05-18 20:27:51 +0300

    feat: перекопировал схему из V2

--- app/schemas/returnschema.py
+++ app/schemas/returnschema.py
@@ -2,8 +2,13 @@ from pydantic import BaseModel
 
 from typing import Optional, Any
 
-class ReturnForm(BaseModel):
+class ReturnForm(BaseModel): #Универсальный
     success: bool
     message: str
-    data: Optional[Any] = None
     error_code: Optional[str] = None
+    data: Optional[Any] = None
+    total: Optional[int] = None
+    page: Optional[int] = None
+    total_pages: Optional[int] = None
+    limit: Optional[int] = None
+    id: Optional[int] = None
