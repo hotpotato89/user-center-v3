f93cefdb8594d64fd83173590eafdffde57bd349
hotpotato <ramilreading@gmail.com>
2026-05-19 20:25:44 +0300

    feat: сделал тест на добавление

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,35 @@ import pytest
 from fastapi.testclient import TestClient
 from faker import Faker
 from random import randint
+import asyncpg
 
 from run import app
 from app.core.config import settings
+from app.core.database import init_db, DSN
 
 fake = Faker()
 
+@pytest.fixture(scope='session')
+async def pool_create():
+    pool = await asyncpg.create_pool(DSN, min_size=1, max_size=2)
+
+    async with pool.acquire() as conn:
+        await init_db(pool)
+    
+    yield pool
+
+    async with pool.acquire() as conn:
+        await conn.execute('truncate users restart identity')
+    await pool.close()
+
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(pool_create):
+    app.state.pool = pool_create
+
+    with TestClient(app) as client:
+        yield client
+    
+    app.state.pool = None
 
 @pytest.fixture
 def password():
