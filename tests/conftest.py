cf19046dc3a7e8116186158e4f9442c955cf6f47
hotpotato <ramilreading@gmail.com>
2026-05-19 17:11:01 +0300

    feat: добавил фикструру для пароля

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,12 @@ from fastapi.testclient import TestClient
 
 from run import app
 
+from app.core.config import settings
+
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def password():
+    return settings.admin_password
