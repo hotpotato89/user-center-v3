608e312f2d18dda9082ba8ab50e015b0b9d7abd0
hotpotato <ramilreading@gmail.com>
2026-05-19 21:03:18 +0300

    feat: добавил test_get_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,3 +8,11 @@ async def test_add_user(session, url, test_user):
         assert 'success' in data
         assert 'message' in data
         assert 'id' in data
+
+@pytest.mark.asyncio
+async def test_get_users(session, url):
+    async with session.get(f'{url}/users?limit=1&page=1') as resp:
+        assert resp.status == 200
+        data = await resp.json()
+        assert data['success'] == True
+        assert 'data' in data
