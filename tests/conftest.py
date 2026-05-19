389c938ce88a84a7fc416960230f69419a9a9bf9
hotpotato <ramilreading@gmail.com>
2026-05-19 19:55:08 +0300

    feat: добавил Faker для тест пользователей

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
+from faker import Faker
+from random import randint
 
 from run import app
-
 from app.core.config import settings
+
+fake = Faker()
 
 @pytest.fixture
 def client():
@@ -12,3 +15,12 @@ def client():
 @pytest.fixture
 def password():
     return settings.admin_password
+
+@pytest.fixture
+def fake_user():
+    data = {
+        'name': fake.first_name(),
+        'age': randint(1, 120),
+        'email': fake.email()
+    }
+    return data
