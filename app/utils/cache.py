3a672ba1209da808c2c412c282ab204b9e95583e
hotpotato <ramilreading@gmail.com>
2026-05-20 20:16:37 +0300

    feat: внедрил логгер в декоратор

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -4,6 +4,10 @@ from redis.asyncio import Redis
 from fastapi import Request
 from typing import Callable
 
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 def cache(ttl: int = 60):
     def wrapper(func: Callable):
         @wraps(func)
@@ -21,6 +25,7 @@ def cache(ttl: int = 60):
                         break
             
             if not request:
+                logger.warning('Request не обнаружен')
                 return await func(*args, **kwargs)
             
             redis: Redis = request.app.state.redis
@@ -28,13 +33,16 @@ def cache(ttl: int = 60):
 
             cached = await redis.get(cache_key)
             if cached:
+                logger.info(f'Cache HIT: {cache_key}')
                 return json.loads(cached)
             
             result = await func(*args, **kwargs)
+            logger.info(f'Cache MISS: {cache_key}')
 
             if result:
                 data = result.dict() if hasattr(result, 'dict') else result
                 await redis.setex(cache_key, ttl, json.dumps(data, default=str))
+                logger.info(f'Cache saved: {cache_key}')
             
             return result
         return inner
