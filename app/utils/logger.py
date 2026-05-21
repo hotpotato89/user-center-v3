ec5fc83052dab2bd5d24a28645d6ec5803b92b8c
hotpotato <ramilreading@gmail.com>
2026-05-21 13:27:46 +0300

    fix: исправил оформление фремени на :

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -14,7 +14,7 @@ def get_logger(name: str) -> logging.Logger:
 
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s -> %(levelname)s: %(message)s',
-        datefmt='%Y-%m-%d %H-%M-%S'
+        datefmt='%Y-%m-%d %H:%M:%S'
     )
 
     handler.setFormatter(formatter)
